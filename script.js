cb09dbaecc436da604eb80374c7d065c5c41864f
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-17 13:48:48 +0000

    reformated my java scipt

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function getInputValue( ){ 
+function getInputValue () {
   // Selecting the input element and get its value
-  var inputVal = document.getElementById('text').value
+  let inputVal = document.getElementById('text').value
   // Displaying the value
   alert(inputVal)
-  }
-//detects wwhen button is pressed and activates the function
-document.getElementById("button").addEventListener("click", getInputValue)
+}
+// detects wwhen button is pressed and activates the function
+document.getElementById('button').addEventListener('click', getInputValue)
