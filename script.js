f96d237dc5bcd3437c5c94c8f5974732752c299e
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-17 13:29:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function getInputValue(){
+  // Selecting the input element and get its value 
+  var inputVal = document.getElementById("text").value;
+  // Displaying the value
+  alert(inputVal);
+  }
+  
